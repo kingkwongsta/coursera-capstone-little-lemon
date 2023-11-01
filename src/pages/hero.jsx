569bc8de33a58cb170fe2c488e562762a73b6e25
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-01 15:21:17 -0700

    add content and styling to hero

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HeroImage from "./../images/hero.jpg";
 
 function Hero() {
   return (
-    <div className="flex flex-row">
-      <div className="w-3/5 border-solid border-2 border-sky-500">
+    <div className="flex flex-row items-center">
+      <div className="w-3/5 border-solid border-2 border-sky-500 justify-center p-8">
         <h2>Little Lemon</h2>
         <p>
           Based in Chicago, Illinois, Little Lemon is a family-owned
@@ -15,7 +16,13 @@ function Hero() {
           day.
         </p>
       </div>
-      <div className="w-2/5 border-solid border-2 border-sky-500">2</div>
+      <div className="w-2/5 border-solid border-2 border-sky-500 flex justify-center p-4">
+        <img
+          className="max-h-[400px] rounded-3xl"
+          src={HeroImage}
+          alt="hero-food"
+        />
+      </div>
     </div>
   );
 }
