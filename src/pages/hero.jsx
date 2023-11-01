61e323e00a8d6c6ea48517bc5fd6fe0d192182e0
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-01 15:26:16 -0700

    more styling

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -3,10 +3,12 @@ import HeroImage from "./../images/hero.jpg";
 
 function Hero() {
   return (
-    <div className="flex flex-row items-center">
-      <div className="w-3/5 border-solid border-2 border-sky-500 justify-center p-8">
-        <h2>Little Lemon</h2>
-        <p>
+    <div className="flex flex-row items-center bg-lime-800">
+      <div className="w-3/5 justify-center p-8 mx-5">
+        <h2 className="text-5xl text-[#f4ce14] font-medium mb-8">
+          Little Lemon
+        </h2>
+        <p className="text-lg text-white font-light">
           Based in Chicago, Illinois, Little Lemon is a family-owned
           Mediterranean restaurant, focused on traditional recipes served with a
           modern twist. The chefs draw inspiration from Italian, Greek, and
@@ -16,9 +18,9 @@ function Hero() {
           day.
         </p>
       </div>
-      <div className="w-2/5 border-solid border-2 border-sky-500 flex justify-center p-4">
+      <div className="w-2/5 flex justify-center p-4">
         <img
-          className="max-h-[400px] rounded-3xl"
+          className="max-h-[350px] rounded-3xl"
           src={HeroImage}
           alt="hero-food"
         />
