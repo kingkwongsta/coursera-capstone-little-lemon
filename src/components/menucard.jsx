5bdc2b5e4993f6eb15337379924ae064e001dd76
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-02 10:18:48 -0700

    category of menu items

--- src/components/menucard.jsx
+++ src/components/menucard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-function MenuCard() {
+function MenuCard({ cardimg, menuitems, category }) {
   return (
     <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
       <div className="relative flex justify-center">
         <img
           className="rounded-[20px] flex-shrink-0 max-h-[300px]"
-          src={MenuImage}
+          src={cardimg}
           alt="flight bgd"
         />
         <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
-          Appetizers
+          {category}
         </h2>
       </div>
       <div className="menu-items flex justify-center">
         <ul className="mt-5">
-          {appetizer.map((item, index) => {
+          {menuitems.map((item, index) => {
             return (
               <li key={index} className="text-[18px] my-[15px]">
                 {item}
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MenuImage from "./../images/menu.jpg";
+import MenuCard from "../components/menucard";
 
 const appetizer = [
   "Whipped Feta with Cucumbers",
@@ -26,29 +27,11 @@ function Menu() {
   return (
     <div className="flex flex-row items-center px-[70px] mt-[50px]">
       {/* APPS */}
-      <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
-        <div className="relative flex justify-center">
-          <img
-            className="rounded-[20px] flex-shrink-0 max-h-[300px]"
-            src={MenuImage}
-            alt="flight bgd"
-          />
-          <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
-            Appetizers
-          </h2>
-        </div>
-        <div className="menu-items flex justify-center">
-          <ul className="mt-5">
-            {appetizer.map((item, index) => {
-              return (
-                <li key={index} className="text-[18px] my-[15px]">
-                  {item}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
+      <MenuCard
+        cardimg={MenuImage}
+        menuitems={appetizer}
+        category="Appetizers"
+      />
       {/* MAINS */}
       <div className="main flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
         <div className="relative flex justify-center">
