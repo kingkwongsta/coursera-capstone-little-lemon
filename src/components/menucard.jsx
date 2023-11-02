0d26728d49cc055d402fa2b993d6d77908f5feff
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-02 10:11:42 -0700

    setup menu card component

---
+++ src/components/menucard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function MenuCard() {
+  return (
+    <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
+      <div className="relative flex justify-center">
+        <img
+          className="rounded-[20px] flex-shrink-0 max-h-[300px]"
+          src={MenuImage}
+          alt="flight bgd"
+        />
+        <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
+          Appetizers
+        </h2>
+      </div>
+      <div className="menu-items flex justify-center">
+        <ul className="mt-5">
+          {appetizer.map((item, index) => {
+            return (
+              <li key={index} className="text-[18px] my-[15px]">
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default MenuCard;
