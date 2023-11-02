6f167c7ac3474f395dbd087afa45b9f787504464
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-02 10:09:18 -0700

    provide data for entrees and desserts

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -8,19 +8,32 @@ const appetizer = [
   "Zeytinyağli Yaprak Sarmasi",
   "Summer Bulgur and Green Bean Salad",
 ];
+const entrees = [
+  "Chicken Breasts with Artichoke-Olive Sauce",
+  "Sea Bass with Tomato and Black Olive Salsa",
+  "Greek Fish Stew",
+  "Spiced Pork Tenderloin with Hazelnut Vinaigrette",
+];
+const desserts = [
+  "Baklava",
+  "Tiramisu",
+  "Loukoumades",
+  "Galaktoboureko",
+  "Pastéis de Nata",
+];
 
 function Menu() {
   return (
     <div className="flex flex-row items-center px-[70px] mt-[50px]">
       {/* APPS */}
-      <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px]">
+      <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
         <div className="relative flex justify-center">
           <img
             className="rounded-[20px] flex-shrink-0 max-h-[300px]"
             src={MenuImage}
             alt="flight bgd"
           />
-          <h2 className="absolute bottom-[90px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
+          <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
             Appetizers
           </h2>
         </div>
@@ -37,20 +50,20 @@ function Menu() {
         </div>
       </div>
       {/* MAINS */}
-      <div className="main flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px]">
+      <div className="main flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
         <div className="relative flex justify-center">
           <img
             className="rounded-[20px] flex-shrink-0 max-h-[300px]"
             src={MenuImage}
             alt="flight bgd"
           />
-          <h2 className="absolute bottom-[90px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
-            Appetizers
+          <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
+            Entrees
           </h2>
         </div>
         <div className="menu-items flex justify-center">
           <ul className="mt-5">
-            {appetizer.map((item, index) => {
+            {entrees.map((item, index) => {
               return (
                 <li key={index} className="text-[18px] my-[15px]">
                   {item}
@@ -61,20 +74,20 @@ function Menu() {
         </div>
       </div>
       {/* DESSERTS */}
-      <div className="dessert flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px]">
+      <div className="dessert flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
         <div className="relative flex justify-center">
           <img
             className="rounded-[20px] flex-shrink-0 max-h-[300px]"
             src={MenuImage}
             alt="flight bgd"
           />
-          <h2 className="absolute bottom-[90px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
-            Appetizers
+          <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
+            Desserts
           </h2>
         </div>
         <div className="menu-items flex justify-center">
           <ul className="mt-5">
-            {appetizer.map((item, index) => {
+            {desserts.map((item, index) => {
               return (
                 <li key={index} className="text-[18px] my-[15px]">
                   {item}
