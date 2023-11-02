6039b49c75f93a5cf2d71e3f35c2bfb73e2fe6e4
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-02 10:27:30 -0700

    move cat text to top of img

--- src/components/menucard.jsx
+++ src/components/menucard.jsx
@@ -2,16 +2,19 @@ import React from "react";
 
 function MenuCard({ cardimg, menuitems, category }) {
   return (
-    <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[566px]">
-      <div className="relative flex justify-center">
+    <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[580px]">
+      <div className="flex flex-col justify-center">
+        <h2 className="text-center font-[500] text-[25px] text-[#f4ce14] leading-[51px] mb-2">
+          {category}
+        </h2>
         <img
           className="rounded-[20px] flex-shrink-0 max-h-[300px]"
           src={cardimg}
           alt="flight bgd"
         />
-        <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
+        {/* <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
           {category}
-        </h2>
+        </h2> */}
       </div>
       <div className="menu-items flex justify-center text-center">
         <ul className="mt-5">
