804bf83535cb3ed0e1b99c48c73be0becf27f1de
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-14 14:34:01 -0800

    add navbar to menu route

--- src/routes/MenuRoute.jsx
+++ src/routes/MenuRoute.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Menu from "./../pages/menu";
+import NavBar from "./../components/navbar";
 
 function MenuRoute() {
-  return <Menu />;
+  return (
+    <>
+      <NavBar />
+      <Menu />
+    </>
+  );
 }
 
 export default Menu;
