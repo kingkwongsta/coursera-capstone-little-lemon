709b2f29ad6a6b3a76ee8ae8527836becb1bf4d7
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-14 14:25:11 -0800

    wrap app in react router provider

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
 
