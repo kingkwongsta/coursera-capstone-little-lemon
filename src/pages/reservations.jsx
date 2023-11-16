f8994a4cbe3b036bee009f613fb015230602ab3d
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-16 10:39:30 -0800

    dependency for forms and data picker installed

--- src/pages/reservations.jsx
+++ src/pages/reservations.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ResyImage from "./../images/resy.jpg";
+import ResyForm from "../components/resyform";
 
 function Reservations() {
   return (
-    <div className="flex flex-col">
-      <div>Hello</div>
-      <div>World</div>
+    <div className="flex flex-row justify-around mt-12">
+      <div>
+        <ResyForm />
+      </div>
+      <div>
+        <img
+          className="max-h-[400px] rounded-xl"
+          src={ResyImage}
+          alt="reservation headline"
+        />
+      </div>
     </div>
   );
 }
