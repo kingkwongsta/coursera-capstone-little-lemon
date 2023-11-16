9cb4766124cb85b624710afcba391eb9aa530c24
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-16 11:44:48 -0800

    padding of about section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import AboutImage from "./../images/about.jpg";
 
 function About() {
   return (
-    <div className="flex flex-row items-center bg-slate-400 mt-5">
+    <div className="flex flex-row items-center bg-slate-400 mt-5 px-16">
       <div className="w-2/5 flex justify-center p-4 pr-[70px]">
         <img
           className="max-h-[350px] rounded-3xl"
