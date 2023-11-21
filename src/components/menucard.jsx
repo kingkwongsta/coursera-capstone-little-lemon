7a8801aacd8f24ab9f7ab1b5027a5df4645eb8f5
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-20 16:57:34 -0800

    fix repsonsive with max

--- src/components/menucard.jsx
+++ src/components/menucard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MenuCard({ cardimg, menuitems, category }) {
   return (
-    <div className="apps flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[580px] md:my-5">
+    <div className="menu-card flex-auto bg-slate-200 max-w-[450px] p-5 mx-10 rounded-[20px] min-h-[580px] md:m-[50px]">
       <div className="flex flex-col justify-center items-center">
         <h2 className="text-center font-[500] text-[25px] text-[#f4ce14] leading-[51px] mb-2">
           {category}
@@ -12,9 +12,6 @@ function MenuCard({ cardimg, menuitems, category }) {
           src={cardimg}
           alt="flight bgd"
         />
-        {/* <h2 className="absolute bottom-[110px] w-full text-center font-[500] text-[25px] leading-[51px] text-[#FFFFFF] z-10">
-          {category}
-        </h2> */}
       </div>
       <div className="menu-items flex justify-center text-center">
         <ul className="mt-5">
