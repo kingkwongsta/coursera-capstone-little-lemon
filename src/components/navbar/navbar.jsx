71893111e4d7a1d9507f1c931b6c307dadda4c3b
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-21 14:09:33 -0800

    separate responsive menues out

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./../../images/logo.png";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    console.log(menuOpen);
+  };
+
   return (
-    <nav className="flex items-center justify-between px-[90px] py-2 bg-slate-100">
-      <div className="">
+    <nav className="flex items-center justify-between px-4 py-2 md:px-6 bg-slate-100">
+      <div className="flex items-center">
         <a href="/">
-          <img className="max-h-[50px]" src={Logo} alt="little-lemon-logo" />
+          <img className="max-h-10" src={Logo} alt="little-lemon-logo" />
         </a>
       </div>
-      <div className="flex space-x-6">
+      {/* Hamburger menu icon - Responsive Layout */}
+      <div className="md:hidden">
+        <button
+          onClick={handleToggleMenu}
+          className="text-gray-500 hover:text-[#f4ce14] focus:outline-none"
+        >
+          <svg
+            className="h-6 w-6 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+        <div className={`${menuOpen ? "flex flex-col" : "hidden"}`}>
+          <a href="/" className="text-gray-500 hover:text-[#f4ce14]">
+            Home
+          </a>
+          <a href="/about" className="text-gray-500 hover:text-[#f4ce14]">
+            About
+          </a>
+          <a href="/menu" className="text-gray-500 hover:text-[#f4ce14]">
+            Menu
+          </a>
+          <a
+            href="/reservations"
+            className="text-gray-500 hover:text-[#f4ce14]"
+          >
+            Reservations
+          </a>
+        </div>
+      </div>
+      {/* Menu Large Screen Display */}
+      <div className="flex flex-row">
         <a href="/" className="text-gray-500 hover:text-[#f4ce14]">
           Home
         </a>
