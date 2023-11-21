026380dee169548df3ec6a24490ccdcc03fad0b4
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-11-20 19:46:45 -0800

    install file loader and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
   transformIgnorePatterns: ["/node_modules/", "^.+\\.css$"],
+  moduleNameMapper: {
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+  },
 };
